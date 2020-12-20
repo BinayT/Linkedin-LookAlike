e406cbe9a1d53491b01f982c786bac59969d7fdb
Binay <binayprogramming@gmail.com>
2020-12-20 14:06:17 +0100

    Basic redux is connected to the app and working fine now

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import './App.scss';
 import Feed from './components/Feed/Feed';
 import Header from './components/Header/Header';
+import Login from './components/Login/Login';
 import Sidebar from './components/Sidebar/Sidebar';
+import { selectUser } from './features/userSlice';
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className='app'>
-      {/* Header */}
       <Header />
-      {/* Sidebar */}
-      <div className='app__body'>
-        <Sidebar />
-        <Feed />
-        {/* Widget */}
-      </div>
+
+      {user ? (
+        <div className='app__body'>
+          <Sidebar />
+          <Feed />
+        </div>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 }
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Login() {
+  return <div>Hey</div>;
+}
+
+export default Login;
